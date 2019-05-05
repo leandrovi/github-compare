707eceff53ebb9782c3f9dd971671bc93cbc9716
leandrovi <leandrofernandesvieira@gmail.com>
2019-05-05 19:29:38 -0300

    Exibir erro no input

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -10,6 +10,7 @@ import CompareList from '../../components/CompareList';
 
 export default class Main extends Component {
   state = {
+    repositoryError: false,
     repositoryInput: '',
     repositories: [],
   };
@@ -26,21 +27,22 @@ export default class Main extends Component {
       repository.lastCommit = moment(repository.pushed_at).fromNow();
 
       this.setState({
+        repositoryError: false,
         repositoryInput: '',
         repositories: [...repositories, repository],
       });
     } catch (err) {
-      console.log(err);
+      this.setState({ repositoryError: true });
     }
   };
 
   render() {
-    const { repositoryInput, repositories } = this.state;
+    const { repositoryError, repositoryInput, repositories } = this.state;
     return (
       <Container>
         <img src={logo} alt="GitHub Compare" />
 
-        <Form onSubmit={this.handleAddRespository}>
+        <Form withError={repositoryError} onSubmit={this.handleAddRespository}>
           <input
             type="text"
             placeholder="usuário/repositório"
